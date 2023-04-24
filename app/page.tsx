e452b6437b69ef16da1f1b981e61d9757d67759a
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-04-24 20:08:33 +0530

    Added full blog app

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import Posts from "./components/Posts";
+
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I&apos;m <span className="font-bold">Shan</span>.
+          I&apos;m <span className="font-bold">Cat</span>.
         </span>
       </p>
+      <Posts />
     </main>
   );
 }
